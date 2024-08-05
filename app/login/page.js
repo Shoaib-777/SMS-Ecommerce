1fa98621f3c2868e012d93e5c10ae7f1e2c072f6
Shoaib-777 <164485277+Shoaib-777@users.noreply.github.com>
2024-08-05 10:30:14 +0530

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -4,12 +4,17 @@ import { LuEye, LuEyeOff } from "react-icons/lu";
 import { useState } from "react";
 import Link from "next/link";
 import { toast } from "react-toastify";
+import {useFormState} from 'react-dom'
+import { authenticate } from "../utils/crud";
+
 const Login = () => {
+    const [state, formAction] = useFormState(authenticate, undefined);
     const [show, setShow] = useState(false)
     const handleLogin = ()=> toast.error("Sorry Can't Login Now")
     return (
         <div className="background w-full h-full">
-            <div className="mx-auto border border-gray-200 w-[370px] h-[550px] sm:w-[400px] sm:h-[550px] px-6 mt-[1rem] rounded-lg bg-transparent mb-[3rem] ">
+            <div className="mx-auto border border-gray-200 w-[370px] h-[560px] sm:w-[400px] sm:h-[570px] px-6 mt-[1rem] rounded-lg bg-transparent mb-[3rem] ">
+                <form action={formAction}>
                 <div className=" mt-[2rem] ">
                     <h2 className="text-center text-white font-bold text-2xl">Login</h2>
                 </div>
@@ -31,7 +36,7 @@ const Login = () => {
                     }
                 </div>
                 <div className="mb-2">
-                    <button onClick={handleLogin} className="w-full text-center py-2 border border-white rounded-md bg-black text-white font-bold  ">Login</button>
+                    <button  className="w-full text-center py-2 border border-white rounded-md bg-black text-white font-bold  ">Login</button>
                 </div>
                 <div className="flex items-center justify-center w-full mb-2 mt-2 ">
                     <div className="border border-white w-[37%] mr-1 "> </div>
@@ -41,14 +46,15 @@ const Login = () => {
                     <div className="border border-white w-[37%] ml-1"> </div>
                 </div>
                 <div className="relative mt-2">
-                    <button onClick={handleLogin} className="w-full py-2  border border-white rounded-md bg-white text-black font-bold text-[16px] tracking-tighter ">Continue With Google</button>
+                    <button onClick={handleLogin}  className="w-full py-2  border border-white rounded-md bg-white text-black font-bold text-[16px] tracking-tighter ">Continue With Google</button>
                     <FcGoogle className=" w-4 h-4 absolute top-3 left-[65px] cursor-pointer " />
                 </div>
                 <div className="mt-4">
                     <h3 className="text-white font-bold text-[16px] text-center ">Don&apos;t have account <Link href={'/signup'}><span className="text-purple-700 cursor-pointer hover:underline decoration-[2px] "> Signup?</span></Link> </h3>
                 </div>
-               
-            </div>
+                <h3 className={`${state && state === 'Wrong Credentials!' ? 'bg-red-600': 'bg-green-500 text-white'} font-bold text-center text-xl mt-2`}>{state && state}</h3>
+                </form>
+               </div>
         </div>
     )
 }
